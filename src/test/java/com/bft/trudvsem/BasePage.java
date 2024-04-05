d6667e06bf47cabbda27077682e8e44dbbe75c2f
Falin Pavel <p.falin@bftcom.com>
2024-04-05 11:36:10 +0300

    page object, new class BaseTest, new file conf.properties, new class ConfProperties

--- src/test/java/com/bft/trudvsem/BasePage.java
+++ src/test/java/com/bft/trudvsem/BasePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-public class BasePage {
+public class BasePage extends LoginPageTest { // добавил extends
     public WebDriver driver;
     public BasePage(WebDriver driver) {
         PageFactory.initElements(driver,this);
@@ -12,21 +12,52 @@ public class BasePage {
     }
     // Нажатие на кнопку "Войти" по XPath
     @FindBy(xpath = "//body//div[@class='mega-menu__user-interface']//a")
-    private WebElement loginButton;
+    private WebElement loginBaseButton; // может без private???
     // Нажатие на кнопку входа через госуслуги
     @FindBy(xpath = "//body//main//div[@class='mb-3 row']//button")
-    private WebElement loginEsia;
+    private WebElement loginEsia; // может без private???
     // Ввод логина на форме авторизации ЕСИА
     @FindBy(xpath = "//*[@id='login']")
-    private WebElement setLoginEsia;
+    private WebElement inputLoginEsia; // может без private???
     // Ввод пароля на форме авторизации ЕСИА
     @FindBy(xpath = "//*[@id='password']")
-    private WebElement setPasswordEsia;
+    private WebElement inputPasswordEsia; // может без private???
     // Нажатие на кнопку "Войти" на форме авторизации ЕСИА
     @FindBy(xpath = "//body//div//button[@class='plain-button plain-button_wide']")
-    private WebElement clickEnter;
+    private WebElement clickEnter; // может без private???
     // Нажать на кнопку "individual_button" (соискатель)
     @FindBy(xpath = "//*[@id='individual_button']")
-    private WebElement clickSoiskatel;
+    private WebElement clickApplicant; // может без private???
+
+    // МЕТОДЫ
+    // простой клик на кнопку"Войти"
+    public void clickloginBaseButton () {
+        loginBaseButton.click();
+    }
+
+    // простой клик на кнопку "Войти через Госуслуги"
+    public void clickloginEsia () {
+        loginEsia.click();
+    }
+
+    // Ввод логина ЕСИА
+    public void inputLoginEsia (String login) {
+        inputLoginEsia.sendKeys(login);
+    }
+
+    // ввод пароля ЕСИА
+    public void inputPasswordEsia (String password) {
+        inputPasswordEsia.sendKeys(password);
+    }
+
+    // Клик на кнопку "Войти" в ЕСИА
+    public void clickEnter () {
+        clickEnter.click();
+    }
+
+    // Клик на кнопку "Соискатель"
+    public void clickApplicant () {
+        clickApplicant.click();
+    }
 
 }
--- src/test/java/com/bft/trudvsem/LoginPageTest.java
+++ src/test/java/com/bft/trudvsem/LoginPageTest.java
@@ -1,8 +1,6 @@
 package com.bft.trudvsem;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,6 +14,10 @@ public class LoginPageTest {
     public static WebDriver driver;
     public static WebDriverWait wait;
 
+    @BeforeAll
+    public static void start() {
+        System.out.println("Начало выполнения теста");
+    }
     @BeforeEach
     public void setUp() {
         //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
@@ -30,40 +32,61 @@ public class LoginPageTest {
         BasePage = new BasePage(driver);
     }
     @Test
-    public void loginTest() {
+    public void loginTest () {
         // Открытие страницы
-        //driver.get("https://web-hotfix-test11.k8s.trudvsem.ru/");
+        //driver.get("https://web-hotfix-test11.k8s.trudvsem.ru/"); // подключили в сетап через проперти
+
         // Нажатие на кнопку "Войти" по XPath
-        WebElement loginButton = driver.findElement(By.xpath("//body//div[@class='mega-menu__user-interface']//a"));
-        loginButton.click();
+        //WebElement loginButton = driver.findElement(By.xpath("//body//div[@class='mega-menu__user-interface']//a"));
+        //loginButton.click();
+        BasePage.clickloginBaseButton();
+
         // Нажатие на кнопку входа через госуслуги
-        WebElement submitButton = driver.findElement(By.xpath("//body//main//div[@class='mb-3 row']//button"));
-        submitButton.click();
+        //WebElement submitButton = driver.findElement(By.xpath("//body//main//div[@class='mb-3 row']//button"));
+        //submitButton.click();
+        BasePage.clickloginEsia();
+
         // Дожидаемся загрузки страницы ввода логина и пароля
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='login']")));
+
         // Ввод логина на форме авторизации ЕСИА
-        WebElement loginField = driver.findElement(By.xpath("//*[@id='login']"));
-        loginField.sendKeys("+7(929)8405043");
+        //WebElement loginField = driver.findElement(By.xpath("//*[@id='login']"));
+        //loginField.sendKeys("+7(929)8405043");
+        BasePage.inputLoginEsia(ConfProperties.getProperty("login"));
+
         // Ввод пароля на форме авторизации ЕСИА
-        WebElement passwordField = driver.findElement(By.xpath("//*[@id='password']"));
-        passwordField.sendKeys("Y3a!5eT$");
+        //WebElement passwordField = driver.findElement(By.xpath("//*[@id='password']"));
+        //passwordField.sendKeys("Y3a!5eT$");
+        BasePage.inputPasswordEsia(ConfProperties.getProperty("password"));
+
         // Нажатие на кнопку "Войти" на форме авторизации ЕСИА
-        WebElement loginSubmitButton = driver.findElement(By.xpath("//body//div//button[@class='plain-button plain-button_wide']"));
-        loginSubmitButton.click();
+        //WebElement loginSubmitButton = driver.findElement(By.xpath("//body//div//button[@class='plain-button plain-button_wide']"));
+        //loginSubmitButton.click();
+        BasePage.clickEnter();
+
         // Дождаться загрузки страницы
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='individual_button']")));
+
         // Нажать на кнопку "individual_button"
-        WebElement individualButton = driver.findElement(By.xpath("//*[@id='individual_button']"));
-        individualButton.click();
+        //WebElement individualButton = driver.findElement(By.xpath("//*[@id='individual_button']"));
+        //individualButton.click();
+        BasePage.clickApplicant();
+
         // Дождаться полной загрузки страницы
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='ib-display_templates']/h1")));
+
         // Проверка, что страница успешно загружена
         //WebElement pageTitle = driver.findElement(By.xpath("//div[@class='ib-display_templates']/h1"));
         //assert pageTitle.getText().equals("Мой кабинет");
     }
-        @AfterEach
-        public void tearDown() {
-            // Закрытие браузера
-            driver.quit();
+    @AfterEach
+    public void tearDown() {
+        // Закрытие браузера
+        driver.quit();
+
+    }
+    @AfterAll
+    public static void end() {
+        System.out.println("Все тесты успешны");
     }
 }
